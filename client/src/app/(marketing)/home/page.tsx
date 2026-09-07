dc9e46e0bd43ecfafa94662a95d55bcf3f2dd683
Muhammad Asif <asifahmadd53@gmail.com>
2026-09-08 01:23:12 +0500

    change the hero section image

--- client/src/app/(marketing)/home/page.tsx
+++ client/src/app/(marketing)/home/page.tsx
@@ -97,7 +97,7 @@ export default function HomePage() {
                             <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl  p-4 shadow-lg shadow-zinc-950/15 ring-1">
                                 <Image
                                     className="z-2 w-full border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden"
-                                    src="https://tailark.com/_next/image?url=%2Fmail2-light.png&w=3840&q=75"
+                                    src="https://cdn.21st.dev/assets/mirror/86/867a175524a0966eb144327b962159fa1ac4e1822b0cd857759b754306f3b0d4.png"
                                     // src={dashboard}
                                     alt="app screen"
                                     width="2700"
